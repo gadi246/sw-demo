9b8591740c0d385e4ae0fc7153a0133a880cebb5
gaditzkhori <Gt2017!!>
2018-02-15 18:46:33 +0200

    app code enhance

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+// SW registration
 if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker
@@ -11,60 +12,38 @@ if ('serviceWorker' in navigator) {
 
 }
 
-
-
-// Function to perform HTTP request
-var get = function(url) {
-  return new Promise(function(resolve, reject) {
-
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var result = xhr.responseText
-                result = JSON.parse(result);
-                resolve(result);
-            } else {
-                reject(xhr);
-            }
-        }
-    };
-    
-    xhr.open("GET", url, true);
-    xhr.send();
-
-  }); 
-};
-
+var API_TOKEN = 'TQLTyERYmbbgg866pPbuX3NSXvQb5V4H9behp2ou';
 var root = document.getElementById('root');
-root.innerHTML = '<div class="sk-fading-circle">\n' +
-  '  <div class="sk-circle1 sk-circle"></div>\n' +
-  '  <div class="sk-circle2 sk-circle"></div>\n' +
-  '  <div class="sk-circle3 sk-circle"></div>\n' +
-  '  <div class="sk-circle4 sk-circle"></div>\n' +
-  '  <div class="sk-circle5 sk-circle"></div>\n' +
-  '  <div class="sk-circle6 sk-circle"></div>\n' +
-  '  <div class="sk-circle7 sk-circle"></div>\n' +
-  '  <div class="sk-circle8 sk-circle"></div>\n' +
-  '  <div class="sk-circle9 sk-circle"></div>\n' +
-  '  <div class="sk-circle10 sk-circle"></div>\n' +
-  '  <div class="sk-circle11 sk-circle"></div>\n' +
-  '  <div class="sk-circle12 sk-circle"></div>\n' +
-  '</div>';
+initLoader();
 
-get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=TQLTyERYmbbgg866pPbuX3NSXvQb5V4H9behp2ou')
+// get remote data, fetch API
+
+fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key='+ API_TOKEN)
   .then(function(response) {
-    var image  = new Image();
-    image.onload = function () {
-      root.innerHTML = '';
-      root.appendChild(image);
-    };
-    image.src = response.photos[3].img_src;
-    image.classList.add('targetImage');
+   return response.json();
   })
+  .then(loadImage)
   .catch(function(err) {
     console.log("Error", err);
-  })
+  });
 
+// UI stuff, vanilla rocks!!!
 
+function loadImage(data) {
+  var image  = new Image();
+  image.onload = function () {
+    root.innerHTML = '';
+    root.appendChild(image);
+  };
+  image.src = data.photos[3].img_src;
+  image.classList.add('targetImage');
+}
 
+function initLoader() {
+  var loaderItems = new Array(12).fill(1)
+    .map(function (value, i) {
+      var className = "'sk-circle" + (i + 1) + " sk-circle'";
+      return '  <div class=' + className + '></div>\n';
+    }).join('');
+  root.innerHTML = '<div class="sk-fading-circle">\n' + loaderItems + '</div>';
+}
